6abb3bea5ec91e64329774b290dd313e0b0c3897
anderson.chagas01 <luis.chagas0001@gmail.com>
2023-03-06 10:49:44 -0300

    Adicionar arquivos de projeto.

---
+++ projeto 1/projeto 1.cpp
@@ -0,0 +1,44 @@
+// projeto 1.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	string nome = "";
+	int p1 = 0;
+	int p2 = 0;
+	int media = 0;
+
+
+	cout << "Nome\n";
+	cout << "===============\n";
+	cin >> nome;
+
+
+
+	cout << "Digite a nota da P1\n";
+	cin >> p1;
+
+
+	cout << "Digite a nota da P2\n";
+	cin >> p2;
+
+
+	media = (p1 + p2) / 2;
+
+
+	if (media >= 6)
+	{
+		cout << "Aprovado\n";
+	}
+	else
+	{
+		cout << "Reprovado\n";
+	}
+}
+
+
